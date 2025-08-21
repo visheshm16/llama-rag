b7bf2f02c36124902949e1c1972d443692c96f2f
visheshm16 <mohanty.vishesh2001@gmail.com>
2025-08-21 18:27:50 +0530

    UI tweaks

--- app.py
+++ app.py
@@ -291,4 +291,4 @@ def fetch_response():
     return jsonify({"message": response}), 200
 
 if __name__ == '__main__':
-    app.run(debug=False, host='127.0.0.1', port=8000)
+    app.run(debug=False, host='127.0.0.1', port=8080)
