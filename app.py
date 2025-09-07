cb7ce05422dd212cc615c62d96e270ff12d9fa65
visheshm16 <mohanty.vishesh2001@gmail.com>
2025-09-07 17:59:33 +0530

    region fix for s3 vectors

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ s3vectors = boto3.client(
     "s3vectors",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION")
+    region_name="us-east-1"  # S3 Vectors is only available in us-east-1
 )
 print("S3 Vectors client created ✅")
 
